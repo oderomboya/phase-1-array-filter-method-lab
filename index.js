cd783de54c3d858d8b958bdb725cc92a97ba8412
oderomboya <davismboya3@gmail.com>
2023-03-21 14:59:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching (array,name){
+    return array.filter(
+        function (dons){
+            if (dons.toUpperCase() === name.toUpperCase()){
+            return true;
+            }
+        }
+    )
+}
+
+function fuzzyMatch(array,name){
+    return array.filter(
+        function (dons){
+            if (dons.slice(0, name.length)=== name)
+            return true;
+        }
+    )
+}
+
+
+function matchName (array, name){
+    return array.filter (
+        function (dons){
+            if (dons.name === name){
+            return true;
+            }
+        }
+    )
+}
